0c64c50a4dc909c71d7bf3473b0c53ad7783bfb8
nnguyen609 <nnguyen609@gatech.edu>
2026-05-09 21:57:12 -0400

    Updated main to: read a command, parse it to tokens, start a child process and run a command in it

--- src/main.c
+++ src/main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define INPUT_MAX_LENGTH 1024 // 1KB
 
 int main()
 {
-    char* buffer;
-    char *tokens;
+    char *buffer = NULL;
+    pid_t pid;
     size_t bufsize = INPUT_MAX_LENGTH;
+    char **tokens;
+    char *token;
+    size_t counter = 0;
 
-    tokens = (char*) malloc(bufsize * sizeof(char));
-    buffer = (char*) malloc(bufsize * sizeof(char));
+    tokens = (char **)malloc(bufsize * sizeof(char *));
 
     if (tokens == NULL)
     {
@@ -22,21 +29,62 @@ int main()
     do
     {
         printf("$> ");
-        if (!(getline(&buffer, &bufsize, stdin)))
+        if (getline(&buffer, &bufsize, stdin) == -1)
         {
-            perror("getline");
-            exit(EXIT_FAILURE);
-        }
-        buffer[strcspn(buffer, "\r\n")] = 0; // The function counts the number of characters until it hits a '\r' or a '\n'
-        tokens = strtok(buffer, " ");
-        while (tokens != NULL)
-        {
-            printf("Your input is: %s\n", tokens);
-            tokens = strtok(NULL, " ");
+            if (!feof(stdin))
+            {
+                perror("getline");
+                exit(EXIT_FAILURE);
+            }
+            // This happens when CTRL + D is pressed, so we need to cleanup.
+            free(buffer);
+            free(tokens);
         }
 
+        buffer[strcspn(buffer, "\r\n")] = 0; // The function counts the number of characters until it hits a '\r' or a '\n'
+
+        token = strtok(buffer, " ");
+        while (token != NULL)
+        {
+            tokens[counter] = token;
+            counter++;
+
+            if (counter >= bufsize)
+            {
+                bufsize += INPUT_MAX_LENGTH;
+                tokens = realloc(tokens, bufsize * sizeof(char *));
+            }
+
+            token = strtok(NULL, " ");
+        }
+        tokens[counter] = NULL;
+
+        if (tokens[0] == NULL)
+            continue;
+
+        pid = fork();
+
+        if (pid < 0) // error
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0) // child
+        {
+            if (execvp(tokens[0], tokens) == -1)
+            {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else // parent
+        {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+
+        counter = 0;
+
     } while (1);
-    free(tokens);
-    free(buffer);
     return 0;
 }
